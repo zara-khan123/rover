7df5b870e14025eac671e40f66a6be34f5b407f8
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2022-09-30 18:32:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ roverwidth=100;
 roverheight=90;
 rover_x=10;
 rover_y=10;
+images=["mars 0.jpg", "mars 1.jpg", "mars 2.jfif", "mars 3.jpg, mars 4.jpg"];
+numbers= Math.floor(Math.random()*5);
 
-bg="mars.jpg";
+
+bg=images[numbers];
 console.log(bg);
 rover="rover.png";
 console.log(rover);
@@ -32,6 +35,70 @@ function uploadrover()
 {
 ctx.drawImage(rover_imgTag, rover_x, rover_y, roverwidth, roverheight);
 }
+window.addEventListener("keydown", my_keydown);
 
-uploadrover();
-uploadBackground();
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+if(keyPressed == '87')
+{
+   up();
+}
+if(keyPressed == '83')
+{
+down();
+}
+if(keyPressed == '65')
+{
+   left();
+}
+if(keyPressed =='68')
+{
+    right();
+}
+
+}
+
+function up()
+{
+    if(rover_y >=0)
+    {
+        rover_y = rover_y - 10;
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+function down()
+{
+    if(rover_y <=500)
+    {
+        rover_y = rover_y + 10;
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function left()
+{
+    if(rover_x >=0)
+    {
+        rover_x = rover_x - 10;
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function right()
+{
+    if(rover_x <= 700)
+    {
+        rover_x = rover_x + 10;
+        uploadBackground();
+        uploadrover();
+
+    }
+}
